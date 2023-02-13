9b67dfe5359c8597391c856b9bfbead554d0a338
MaxChuloshnikov <Maks447@ukr.net>
2023-02-13 14:24:45 +0200

    RightSide Done
    ApexCharts

---
+++ src/components/RightSide/RightSide.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import styles from './RightSide.module.css';
+import Updates from '../Updates/Updates';
+import CustomerReview from '../CustomerReview/CustomerReview';
+
+const RightSide = () => {
+  return (
+    <div className={styles.RightSide}>
+       <div>
+            <h3 className={styles.title}>Updates</h3>
+            <Updates/>
+        </div> 
+        <div>
+            <h3>Customer Review</h3>
+            <CustomerReview/>
+        </div>
+    </div>
+  )
+}
+
+export default RightSide
