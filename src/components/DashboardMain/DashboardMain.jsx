cf686aafd49c34f853f29081cbc3aeafcc801d22
MaxChuloshnikov <Maks447@ukr.net>
2023-02-18 16:16:48 +0200

    Layout Done
    Fix need

--- src/components/DashboardMain/DashboardMain.jsx
+++ src/components/DashboardMain/DashboardMain.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Cards from './Cards/Cards';
 import Table from '../Table/Table';
+import styles from './DashboardMain.module.css';
 
 const DashboardMain = () => {
   return (
-    <div>
+    <div className={styles.dashMain}>
         <h1 >Dashboard</h1>
         <Cards/>
         <Table/>
